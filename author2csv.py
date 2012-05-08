48a084a1dcf40de8894db152357615a408209eb4
Ben Companjen <ben@companjen.name>
2012-05-08 17:18:03 +0200

    fixed typos in stats.py, added author exporter and confused records exporter

---
+++ author2csv.py
@@ -0,0 +1,43 @@
+﻿import json
+import fileinput
+import csv
+import sys
+import codecs
+
+# Creates a CSV file from Author records in an Open Library dump file.
+
+# Usage: sed -nrf author2-json.txt <ol_dump> | python author2csv.py authors.csv
+# where author2-json.txt makes sed output the JSON documents, but only for author-type records.
+
+if sys.argv[len(sys.argv)-1] != sys.argv[0]:
+    fn = sys.argv[len(sys.argv)-1]
+    writer = csv.writer(open(fn, 'wb'))
+    writer.writerow(["Author key","Name","Slug"])
+    writer2 = csv.writer(open("noname-"+fn, 'wb'))
+else:
+    sys.exit("No filename supplied!")
+
+
+# Open standard input
+f = fileinput.input('-')
+for line in f:
+    record = json.loads(line)
+    if "name" in record.keys():
+        slug = unicode(record["name"]).lower() # .strip('. /,:;-_\\!@#$%^&*()+=\'".\sʻ')
+        slug = slug.replace('.','')
+        slug = slug.replace(' ','')
+        slug = slug.replace(',','')
+        slug = slug.replace(':','')
+        slug = slug.replace(';','')
+        slug = slug.replace('-','')
+        slug = slug.replace('\'','')
+        slug = slug.replace('"','')
+        #slug = unicode(slug).replace(unicode('ʻ'),'')
+        slug = slug.replace('(','')
+        slug = slug.replace(')','')
+        slug = slug.replace('&','')
+        slug = slug.replace('_','')
+        
+        writer.writerow([unicode(record["key"]).replace('/authors/','').encode('utf-8'), unicode(record["name"]).encode('utf-8'), unicode(slug).encode('utf-8')])
+    else:
+        writer2.writerow([unicode(record["key"]).replace('/authors/','').encode('utf-8')])
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 import json
 import fileinput
 import sys
+import codecs
 
 """Creates a JSON file with statistics.
 
@@ -13,7 +14,7 @@ counted when available.
 
 """
 if sys.argv[len(sys.argv)-1] != sys.argv[0]:
-    outputfile = open(sys.argv[len(sys.argv)-1], 'wb')
+    outputfile = codecs.open(sys.argv[len(sys.argv)-1], 'wb','utf-8')
 else:
     sys.exit("No filename supplied!")
 
@@ -32,48 +33,59 @@ def classifications_stats(record, type):
     """Counts classifications in the record.
     
     """
-    for c in special_class:
+    """for c in special_class:
         if c in record.keys():
             stats[type]["sc"][c][0] = stats[type]["sc"][c][0] + 1
             stats[type]["sc"][c][1] = stats[type]["sc"][c][1] + len(record[c])
-    #if isinstance(record["classifications"], dict):
+            if len(record[c]) == 0:
+                stats[type]["sc"][c][2] = stats[type]["sc"][c][2] + 1
+    """
     for k in record["classifications"].keys():
         if k in stats[type]["classifications"].keys():
             stats[type]["classifications"][k][0] = stats[type]["classifications"][k][0] + 1
             if isinstance(record["classifications"][k], list):
                 stats[type]["classifications"][k][1] = stats[type]["classifications"][k][1] + len(record["classifications"][k])
+                if len(record["classifications"][k]) == 0: # empty list
+                    stats[type]["classifications"][k][2] = stats[type]["classifications"][k][2] + 1
             else:
                 stats[type]["classifications"][k][1] = stats[type]["classifications"][k][1] + 1
         else:
-            stats[type]["classifications"][k][0] = 1
+            stats[type]["classifications"][k] = [1,1,0]
             if isinstance(record["classifications"][k], list):
                 stats[type]["classifications"][k][1] = len(record["classifications"][k])
-            else:
-                stats[type]["classifications"][k][1] = 1
+                if len(record["classifications"][k]) == 0: # empty list
+                    stats[type]["classifications"][k][2] = stats[type]["classifications"][k][2] + 1
                 
-    #else:
-        # classifications are not in a dict
         
 
 def identifiers_stats(record, type):
     """Counts identifiers in the record.
     
     """
-    for i in special_id:
+    # 'top-level' identifiers
+    """for i in special_id:
         if i in record.keys():
             stats[type]["si"][i][0] = stats[type]["si"][i][0] + 1
             stats[type]["si"][i][1] = stats[type]["si"][i][1] + len(record[i])
+            if len(record[i]) == 0:
+                stats[type]["si"][i][2] = stats[type]["si"][i][2] + 1
+    """
+    # 'normal' identifiers
     for k in record["identifiers"].keys():
         if k in stats[type]["identifiers"].keys():
             stats[type]["identifiers"][k][0] = stats[type]["identifiers"][k][0] + 1
             if isinstance(record["identifiers"][k], list):
                 stats[type]["identifiers"][k][1] = stats[type]["identifiers"][k][1] + len(record["identifiers"][k])
+                if len(record["identifiers"][k]) == 0: # empty list
+                    stats[type]["identifiers"][k][2] = stats[type]["identifiers"][k][2] + 1
             else:
                 stats[type]["identifiers"][k][1] = stats[type]["identifiers"][k][1] + 1
         else:
-            stats[type]["identifiers"][k] = [1,1]
+            stats[type]["identifiers"][k] = [1,1,0]
             if isinstance(record["identifiers"][k], list):
                 stats[type]["identifiers"][k][1] = len(record["identifiers"][k])
+                if len(record["identifiers"][k]) == 0: # empty list
+                    stats[type]["identifiers"][k][2] = stats[type]["identifiers"][k][2] + 1
 
 def key_stats(record, type):
     """Counts keys in the record.
@@ -84,10 +96,16 @@ def key_stats(record, type):
             stats[type]["keys"][k][0] = stats[type]["keys"][k][0] + 1
             if isinstance(record[k], list):
                 stats[type]["keys"][k][1] = stats[type]["keys"][k][1] + len(record[k])
+                if len(record[k]) == 0: # empty list
+                    stats[type]["keys"][k][2] = stats[type]["keys"][k][2] + 1
             else:
                 stats[type]["keys"][k][1] = stats[type]["keys"][k][1] + 1
         else:
-            stats[type]["keys"][k] = [1,1]
+            stats[type]["keys"][k] = [1,1,0]
+            if isinstance(record[k], list):
+                stats[type]["keys"][k][1] = len(record[k])
+                if len(record[k]) == 0: # empty list
+                    stats[type]["keys"][k][2] = stats[type]["keys"][k][2] + 1
     
     if "identifiers" in record.keys():
         identifiers_stats(record, type)
@@ -112,6 +130,8 @@ def determine_type(object):
             return "redirect_edition"
         elif object["type"]["key"] == "/type/delete":
             return "deleted_edition"
+        elif object["type"]["key"] == "/type/volume":
+            return "volume"
         else:
             stats["confused"].append((object["type"]["key"],object["key"]))
             return "confused_edition"
@@ -155,8 +175,6 @@ def determine_type(object):
         return "usergroup"
     elif object["type"]["key"] == "/type/i18n_page":
         return "i18n_page"
-    elif object["type"]["key"] == "/type/volume":
-        return "volume"
     else:
         stats["confused"].append((object["type"]["key"],object["key"]))
         return "confused"
@@ -176,15 +194,16 @@ for line in f:
         stats[t]["countr"] = stats[t]["countr"] + 1
     else:
         stats[t] = {"countr": 1, "keys": {}, "classifications": {}, "sc": {}, "identifiers": {}, "si": {}}
-        for c in special_class:
-            stats[t]["sc"][c] = [0,0]
-        for i in special_id:
-            stats[t]["si"][i] = [0,0]
+        #for c in special_class:
+        #    stats[t]["sc"][c] = [0,0,0]
+        #for i in special_id:
+        #    stats[t]["si"][i] = [0,0,0]
         print "new type:", t
     
     try:
         key_stats(record, t)
     except Exception as e:
+        print record["key"], sys.exc_info()
         stats["error"].append((record, str(e)))
 
 json.dump(stats, outputfile, indent=2)

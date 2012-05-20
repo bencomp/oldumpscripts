ac5cbdb45222744d365365457aba49b60db8ad06
Ben Companjen <ben@companjen.name>
2012-05-20 20:58:51 +0200

    added work2csv, updated author2csv to use a function createSlug

--- author2csv.py
+++ author2csv.py
@@ -17,60 +17,38 @@ if sys.argv[len(sys.argv)-1] != sys.argv[0]:
 else:
     sys.exit("No filename supplied!")
 
+def createSlug(input):
+    slug = unicode(input).lower() # .strip('. /,:;-_\\!@#$%^&*()+=\'".\sʻ')
+    slug = slug.replace('.','')
+    slug = slug.replace(' ','')
+    slug = slug.replace(',','')
+    slug = slug.replace(':','')
+    slug = slug.replace(';','')
+    slug = slug.replace('-','')
+    slug = slug.replace('\'','')
+    slug = slug.replace('"','')
+    slug = slug.replace('(','')
+    slug = slug.replace(')','')
+    slug = slug.replace('&','')
+    slug = slug.replace('_','')
+    return slug
 
 # Open standard input
 f = fileinput.input('-')
 for line in f:
     record = json.loads(line)
     if "name" in record.keys():
-        slug = unicode(record["name"]).lower() # .strip('. /,:;-_\\!@#$%^&*()+=\'".\sʻ')
-        slug = slug.replace('.','')
-        slug = slug.replace(' ','')
-        slug = slug.replace(',','')
-        slug = slug.replace(':','')
-        slug = slug.replace(';','')
-        slug = slug.replace('-','')
-        slug = slug.replace('\'','')
-        slug = slug.replace('"','')
-        #slug = unicode(slug).replace(unicode('ʻ'),'')
-        slug = slug.replace('(','')
-        slug = slug.replace(')','')
-        slug = slug.replace('&','')
-        slug = slug.replace('_','')
+        slug = createSlug(record["name"])
         
         dob, dobslug = None, None
         if "birth_date" in record.keys() and record["birth_date"] != "":
             dob = record["birth_date"]
-            dobslug = unicode(dob).lower()
-            dobslug = dobslug.replace('.','')
-            dobslug = dobslug.replace(' ','')
-            dobslug = dobslug.replace(',','')
-            dobslug = dobslug.replace(':','')
-            dobslug = dobslug.replace(';','')
-            dobslug = dobslug.replace('-','')
-            dobslug = dobslug.replace('\'','')
-            dobslug = dobslug.replace('"','')
-            dobslug = dobslug.replace('(','')
-            dobslug = dobslug.replace(')','')
-            dobslug = dobslug.replace('&','')
-            dobslug = dobslug.replace('_','')
+            dobslug = createSlug(dob)
             
         dod, dodslug = None, None
         if "death_date" in record.keys() and record["death_date"] != "":
             dod = record["death_date"]
-            dodslug = unicode(dod).lower()
-            dodslug = dodslug.replace('.','')
-            dodslug = dodslug.replace(' ','')
-            dodslug = dodslug.replace(',','')
-            dodslug = dodslug.replace(':','')
-            dodslug = dodslug.replace(';','')
-            dodslug = dodslug.replace('-','')
-            dodslug = dodslug.replace('\'','')
-            dodslug = dodslug.replace('"','')
-            dodslug = dodslug.replace('(','')
-            dodslug = dodslug.replace(')','')
-            dodslug = dodslug.replace('&','')
-            dodslug = dodslug.replace('_','')
+            dodslug = createSlug(dod)
             
         
         writer.writerow([unicode(record["key"]).replace('/authors/','').encode('utf-8'), unicode(record["name"]).encode('utf-8'), unicode(slug).encode('utf-8'), unicode(dob).encode('utf-8'), unicode(dobslug).encode('utf-8'), unicode(dod).encode('utf-8'), unicode(dodslug).encode('utf-8')])

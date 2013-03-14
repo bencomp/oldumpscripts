f30ea8fec647f406e123ee68607f32d161742676
Ben Companjen <ben@companjen.name>
2013-03-15 00:17:30 +0100

    Update: format code

--- countformats.py
+++ countformats.py
@@ -3,25 +3,28 @@ import fileinput
 import sys
 
 if sys.argv[len(sys.argv)-1] != sys.argv[0]:
-	file = open(sys.argv[len(sys.argv)-1], 'w')
-	file.write("format\t\"total occurrences\"\n")
+    file = open(sys.argv[len(sys.argv)-1], 'w')
+    file.write("format\t\"total occurrences\"\n")
 else:
-	sys.exit("No filename supplied!")
+    sys.exit("No filename supplied!")
 
 countr = {}
+nrformats = 0
 
 # Open standard input
 f = fileinput.input('-')
 for line in f:
-	decoded = json.loads(line)
-	if "physical_format" in decoded:
-		k = decoded["physical_format"]
-		if k in countr.keys():
-			countr[k] = countr[k] + 1
-		else:
-			countr[k] = 1
+    decoded = json.loads(line)
+    if "physical_format" in decoded:
+        k = decoded["physical_format"]
+        if k in countr.keys():
+            countr[k] = countr[k] + 1
+        else:
+            countr[k] = 1
+            nrformats = nrformats + 1
+            print "new format ("+ str(nrformats) +"):", k.encode('utf-8')
 
 keys = countr.keys()
 keys.sort()
 for k in keys:
-	file.write(k.encode('utf-8') + "\t{0}\n".format(countr[k]))
+    file.write(k.encode('utf-8') + "\t{0}\n".format(countr[k]))
--- dump2csv.py
+++ dump2csv.py
@@ -2,7 +2,7 @@
 import fileinput
 import csv
 import sys
-import codecs
+import codecs,cStringIO
 
 # Creates CSV files from an Open Library dump file.
 
@@ -17,16 +17,18 @@ import codecs
 # Usage: sed -nre "s/^[^{]*//p" <ol_dump> | python dump2csv.py <file name prefix>
 
 if sys.argv[len(sys.argv)-1] != sys.argv[0]:
-    of_works = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-works.csv", 'wb','utf-8'))
-    of_authors = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-authors.csv", 'wb','utf-8'))
-    of_editions = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-editions.csv", 'wb','utf-8'))
-    of_works_authors = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-works_authors.csv", 'wb','utf-8'))
-    of_editions_authors = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-editions_authors.csv", 'wb','utf-8'))
-    of_editions_works = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-editions_works.csv", 'wb','utf-8'))
-    of_editions_languages = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-editions_languages.csv", 'wb','utf-8'))
-    of_editions_publishers = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-editions_publishers.csv", 'wb','utf-8'))
+    fnp = sys.argv[len(sys.argv)-1]
+    of_works                = csv.writer(open(fnp+"-works.csv", 'wb'))
+    of_authors              = csv.writer(open(fnp+"-authors.csv", 'wb'))
+    of_editions             = csv.writer(open(fnp+"-editions.csv", 'wb'))
+    of_works_authors        = csv.writer(open(fnp+"-works_authors.csv", 'wb'))
+    of_editions_authors     = csv.writer(open(fnp+"-editions_authors.csv", 'wb'))
+    of_editions_works       = csv.writer(open(fnp+"-editions_works.csv", 'wb'))
+    of_editions_languages   = csv.writer(open(fnp+"-editions_languages.csv", 'wb'))
+    of_editions_publishers  = csv.writer(open(fnp+"-editions_publishers.csv", 'wb'))
     
-    of_errors = csv.writer(codecs.open(sys.argv[len(sys.argv)-1]+"-errors.csv", 'wb','utf-8'))
+    of_errors               = csv.writer(open(fnp+"-errors.csv", 'wb'))
+    of_maxlengths           = open(fnp+"-maxlengths.json", 'wb')
 else:
     sys.exit("No filename prefix supplied!")
 
@@ -64,6 +66,20 @@ def determine_type(object):
         return "language"
     else:
         return "other"
+    
+def handle_error(row):
+    enc(of_errors,row)
+    print row
+
+def enc(file, row):
+    row2 = []
+    for v in row:
+        if isinstance(v, unicode):
+            row2.append(v.encode("utf-8"))
+        else:
+            row2.append(v)
+    
+    file.writerow(row2)
     
 # Open standard input
 f = fileinput.input('-')
@@ -109,11 +125,10 @@ for line in f:
             try:
                 first_work = record["works"][0]["key"].replace('/works/','')
                 for work in record["works"]:
-                    of_editions_works.writerow([key, work["key"].replace('/works/','')])
+                    enc(of_editions_works,[key, work["key"].replace('/works/','')])
             except Exception as e:
-                of_errors.writerow([key, "work", str(e)])
+                handle_error([key, "work", str(e)])
                 num_works = -num_works
-0
                 
         num_authors, first_author = None, None
         if "authors" in record.keys() and len(record["authors"]) != 0:
@@ -121,9 +136,9 @@ for line in f:
             try:
                 first_author = record["authors"][0]["key"].replace('/authors/','')
                 for author in record["authors"]:
-                    of_editions_authors.writerow([key, author["key"].replace('/authors/','')])
+                    enc(of_editions_authors,[key, author["key"].replace('/authors/','')])
             except Exception as e:
-                of_errors.writerow([key, "author", str(e)])
+                handle_error([key, "author", str(e)])
                 num_authors = -num_authors
             
         
@@ -137,23 +152,23 @@ for line in f:
                     ml[t]["publisher"] = max(len(pub), ml[t]["publisher"])
                     pubslug = createSlug(pub)
                     ml[t]["publisherslug"] = max(len(pubslug), ml[t]["publisherslug"])
-                    of_editions_publishers.writerow([key, pub, pubslug])
+                    enc(of_editions_publishers,[key, pub, pubslug])
             except Exception as e:
-                of_errors.writerow(key, "publisher", str(e))
+                handle_error([key, "publisher", str(e)])
         
         if "languages" in record.keys():
             try:
                 for lang in record["languages"]:
-                    of_editions_languages.writerow([key, lang["key"].replace("/lang/", "")])
+                    enc(of_editions_languages,[key, lang["key"].replace("/languages/", "")])
             except Exception as e:
-                of_errors.writerow([key, "language", str(e)])
+                handle_error([key, "language", str(e)])
         
         pubdate = None
         if "publish_date" in record.keys():
             pubdate = record["publish_date"]
             ml[t]["pubdate"] = max(len(pubdate), ml[t]["pubdate"])
         
-        of_editions.writerow([key, title, slug, sub, subslug, num_works, first_work, num_authors, first_author, num_publishers, first_publisher, publisher_slug, pubdate])
+        enc(of_editions,[key, title, slug, sub, subslug, num_works, first_work, num_authors, first_author, num_publishers, first_publisher, publisher_slug, pubdate])
     
     # WORKS
     if t == "work":
@@ -179,12 +194,12 @@ for line in f:
             try:
                 first_author = record["authors"][0]["author"]["key"].replace('/authors/','')
                 for author in record["authors"]:
-                    of_works_authors.writerow([key, author["author"]["key"].replace('/authors/','')])
+                    enc(of_works_authors,[key, author["author"]["key"].replace('/authors/','')])
             except Exception as e:
-                of_errors.writerow([key, "author", str(e)])
+                handle_error([key, "author", str(e)])
                 num_authors = -num_authors
                 
-        of_works.writerow([key, title, slug, sub, subslug, num_authors, first_author])
+        enc(of_works,[key, title, slug, sub, subslug, num_authors, first_author])
     
     # AUTHORS
     if t == "author":
@@ -192,6 +207,8 @@ for line in f:
         
         try:
             slug = createSlug(record["name"])
+            ml[t]["name"] = max(len(record["name"]), ml[t]["name"])
+            ml[t]["slug"] = max(len(slug), ml[t]["slug"])
             
             dob, dobslug = None, None
             if "birth_date" in record.keys() and record["birth_date"] != "":
@@ -208,20 +225,17 @@ for line in f:
                 ml[t]["dodslug"] = max(len(dodslug), ml[t]["dodslug"])
                 
             
-            of_authors.writerow([key, record["name"], slug, dob, dobslug, dod, dodslug])
+            enc(of_authors,[key, record["name"], slug, dob, dobslug, dod, dodslug])
         except Exception as e:
-            of_errors.writerow([key, "general", str(e)])
+            handle_error([key, "general", str(e)])
     
     # general processing
     
     ml["key"] = max(len(key), ml["key"])
     numrecords = numrecords + 1
     if numrecords % 100000 == 0:
-        print numrecords, "processed"
-    
+        print numrecords, "records processed"
 
-print "title_max_length:", title_max_length
-print "subtitle_max_length:", subtitle_max_length
-print "publisher_max_length:", publisher_max_length
-print "pubdate_max_length:", pubdate_max_length
-print "key_max_length:", key_max_length
+print numrecords, "records processed"
+print ml
+simplejson.dump(ml, of_maxlengths)
